a1dfd88cd2764fe063d3fcb72e7a5dd4eee3ffdc
ushibutatory <ushibutatory@gmail.com>
2020-06-21 22:12:52 +0900

    codeformat

--- src/ZundokoSolution/Zundoko.Web/Models/ErrorViewModel.cs
+++ src/ZundokoSolution/Zundoko.Web/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ using Zundoko.Web.Models.Abstracts;
 
 namespace Zundoko.Web.Models
 {
-    public class ErrorViewModel :BaseViewModel
+    public class ErrorViewModel : BaseViewModel
     {
         public string RequestId { get; set; }
 
